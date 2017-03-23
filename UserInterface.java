46e77fbd131817e07d61938dd2cf2b5ed499ae3a
Matthew Wright <mattjgw@gmail.com>
2017-03-22 20:33:50 -0400

    Add files via upload

--- UserInterface.java
+++ UserInterface.java
@@ -4,14 +4,17 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
+@SuppressWarnings("serial")
 public class UserInterface extends JFrame{
 	
+	@SuppressWarnings("rawtypes")
 	private JComboBox parties;
 	private JLabel candidatePicture;
 	private static String[] picture = {"conservative.png", "green.png", "liberal.png", "NDP.png"};
 	private Icon [] symbols = {new ImageIcon(getClass().getResource(picture[0])), new ImageIcon(getClass().getResource(picture[1])), 
 			new ImageIcon(getClass().getResource(picture[2])), new ImageIcon(getClass().getResource(picture[3]))};
 	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public UserInterface()
 	{
 		super("Voting Machine");
