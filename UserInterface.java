6639b3afa1bb9e63fa0b425fe93c73bc167a396e
Matthew Wright <mattjgw@gmail.com>
2017-03-23 19:03:53 -0400

    Add files via upload

--- UserInterface.java
+++ UserInterface.java
@@ -13,7 +13,8 @@ public class UserInterface extends JFrame{
 	private static String[] picture = {"conservative.png", "green.png", "liberal.png", "NDP.png"};
 	private Icon [] symbols = {new ImageIcon(getClass().getResource(picture[0])), new ImageIcon(getClass().getResource(picture[1])), 
 			new ImageIcon(getClass().getResource(picture[2])), new ImageIcon(getClass().getResource(picture[3]))};
-	
+	private JButton vote;
+	private static int[] voteCount = new int [4];
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public UserInterface()
 	{
@@ -21,7 +22,6 @@ public class UserInterface extends JFrame{
 		setLayout(new FlowLayout());
 		
 		parties = new JComboBox(picture);
-		
 		parties.addItemListener(
 				new ItemListener()
 				{
@@ -33,6 +33,19 @@ public class UserInterface extends JFrame{
 				}
 			);
 		
+		vote = new JButton("Caste Vote");
+		vote.addActionListener(
+				new ActionListener()
+				{
+					public void actionPerformed(ActionEvent event)
+					{
+						voteCount[parties.getSelectedIndex()]++;
+						System.out.println(voteCount[parties.getSelectedIndex()]);
+					}
+				}
+				);
+		
+		add(vote);
 		add(parties);
 		candidatePicture = new JLabel(symbols[0]);
 		add(candidatePicture);
