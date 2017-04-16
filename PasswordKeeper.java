bdf3da0242c59b91e5a45ea3b1dc613f72533142
Matthew Wright <mattjgw@gmail.com>
2017-04-16 19:47:28 -0400

    Password Keeper now fully stocked with methods

--- PasswordKeeper.java
+++ PasswordKeeper.java
@@ -1,5 +1,58 @@
 package votingMachine;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class PasswordKeeper {
+	private String path;
+	private Scanner input;
+	@SuppressWarnings("unused")
+	private FileWriter file;
+	private String password;
+	
+	public PasswordKeeper() throws IOException
+	{
+		path = "Election Information";
+		file = new FileWriter(path, true);
+		input = new Scanner(new File(path));
+		if(input.hasNext())
+		{
+			password = input.next();
+		}
+		else
+		{
+			password = "password";
+		}
+		
+	}
+
+	public void storePassword() throws IOException
+	{
+		try
+		{
+			BufferedWriter writer = new BufferedWriter(new FileWriter(path));
+			writer.write(password);
+			System.out.println("Written");
+			writer.flush();
+			writer.close();
+		}
+		catch(IOException e)
+		{
+			e.getStackTrace();
+		}
+	}
+	
+	public void changePassword(String p)
+	{
+		password = p;
+	}
+	
+	public String getPassword()
+	{
+		return password;
+	}
 
 }
